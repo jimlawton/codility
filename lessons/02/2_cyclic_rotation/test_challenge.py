f13fdb827244783edf586b9695393cc3312fddcd
Jim Lawton <jim.lawton@gmail.com>
2018-02-23 23:27:06 +0000

    Add another test case.

--- lessons/02/2_cyclic_rotation/test_challenge.py
+++ lessons/02/2_cyclic_rotation/test_challenge.py
@@ -5,6 +5,7 @@ def test_challenge_array():
     assert solution([3, 8, 9, 7, 6], 3) == [9, 7, 6, 3, 8]
     assert solution([0, 0, 0], 1) == [0, 0, 0]
     assert solution([1, 2, 3, 4], 4) == [1, 2, 3, 4]
+    assert solution([1, 2, 3, 4], 0) == [1, 2, 3, 4]
 
 
 # Benchmark
