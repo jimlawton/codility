a304c843e7e382b40bbba1ab8ff413f71a72eae1
Jim Lawton <jim.lawton@gmail.com>
2018-02-23 23:34:44 +0000

    Simplify.

--- lessons/02/2_cyclic_rotation/challenge.py
+++ lessons/02/2_cyclic_rotation/challenge.py
@@ -2,8 +2,6 @@
 def solution(A, K):
     if K == 0 or K == len(A): 
         return A
-    if K > 1:
-        a = A[K-1:] + A[:K-1]
-    else:
-        a = A[1:] + [A[0]]
-    return a
+    if K == 1:
+        return A[1:] + [A[0]]
+    return A[K-1:] + A[:K-1]
