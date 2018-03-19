8257af0d62657fa83b44a1a88006f48a208bc4d1
Jim Lawton <jim.lawton@gmail.com>
2018-03-19 14:23:11 +0000

    Basic solution, works for example.

--- lessons/03/3_perm_missing_elem/test_challenge.py
+++ lessons/03/3_perm_missing_elem/test_challenge.py
@@ -1,22 +1,11 @@
 
 from challenge import solution
 
-def test_challenge_array():
+def test_challenge():
     # Create empty array (dependent on test)
     # Create single entry array
     # Create an array with the answer at the start
     # Create an array with the answer at the end
     # Create an array with the answer in the middle
-    assert solutionArray([9, 3, 9, 3, 9, 7, 9]) == [9, 3, 9, 3, 9, 7, 9]
-    assert solutionArray([9, 3, 9, 3, 9, 1, 9]) == [9, 3, 9, 3, 9, 1, 9]
-    assert solutionArray([9, 1, 9, 1, 9, 2, 9]) == [9, 1, 9, 1, 9, 2, 9]
-    assert solutionArray([9, 1, 9, 1, 9, 2, 9]) == [9, 1, 9, 1, 9, 2, 9]
     # Single entry slot
-    assert solutionArray([99]) == [99]
-    # Answer in last slot
-    assert solutionArray([2, 2, 3, 3, 4]) == [2, 2, 3, 3, 4]
-    # Answer in first slot
-    assert solutionArray([2, 3, 3, 4, 4]) == [2, 3, 3, 4, 4]
-    # Answer in middle slot
-    assert solutionArray([2, 2, 3, 4, 4]) == [2, 2, 3, 4, 4]
-
+    assert(solution([2, 3, 1, 5]) == 4)
