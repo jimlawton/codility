0ab41763c4772f8eaa67c12f01b17f740278aa5b
Jim Lawton <jim.lawton@gmail.com>
2018-03-19 17:34:57 +0000

    Add single-entry tests.

--- lessons/04/perm_check/test_challenge.py
+++ lessons/04/perm_check/test_challenge.py
@@ -3,6 +3,7 @@ from challenge import solution
 
 
 def test_challenge():
-    # Create empty array (dependent on test)
-    # Create single entry array
-    assert solution([]) == 0
+    assert solution([4]) == 0
+    assert solution([1]) == 1
+    assert solution([4, 1, 3, 2]) == 1
+    assert solution([4, 1, 3]) == 0
