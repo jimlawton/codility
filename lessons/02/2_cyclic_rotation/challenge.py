70eef4e819c509cd1980113a2235292e41868e5c
Jim Lawton <jim.lawton@gmail.com>
2018-03-19 11:20:36 +0000

    Arghhh. Forgot to remove the K=1 special case.

--- lessons/02/2_cyclic_rotation/challenge.py
+++ lessons/02/2_cyclic_rotation/challenge.py
@@ -2,8 +2,6 @@
 def solution(A, K):
     if len(A) == 0 or K == 0 or K == len(A):
         return A
-    if K == 1:
-        return A[1:] + [A[0]]
     K = K % len(A)
     # for i in range(K):
     #     A = [A[-1]] + A[:-1]
