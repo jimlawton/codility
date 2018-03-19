ed9e8d2103406298490ef25a2a13f68b2467a0cb
Jim Lawton <jim.lawton@gmail.com>
2018-03-19 19:52:56 +0000

    Add test.

--- lessons/05/passing_cars/test_challenge.py
+++ lessons/05/passing_cars/test_challenge.py
@@ -5,4 +5,4 @@ from challenge import solution
 def test_challenge():
     # Create empty array (dependent on test)
     # Create single entry array
-    assert solution([]) == 0
+    assert solution([0, 1, 0, 1, 1]) == 5
