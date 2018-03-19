0a56d8bf9f02ff69b979857a2c4137ba0e9d70b1
Jim Lawton <jim.lawton@gmail.com>
2018-03-19 19:55:44 +0000

    Spelling.

--- lessons/05/passing_cars/challenge.py
+++ lessons/05/passing_cars/challenge.py
@@ -2,8 +2,8 @@
 # consecutive elements of array A represent consecutive cars on a road.
 #
 # Array A contains only 0s and/or 1s:
-#  * 0 represents a car traveling east,
-#  * 1 represents a car traveling west.
+#  * 0 represents a car travelling east,
+#  * 1 represents a car travelling west.
 #
 # The goal is to count passing cars. We say that a pair of cars (P, Q), where
 # 0 ≤ P < Q < N, is passing when P is travelling to the east and Q is
