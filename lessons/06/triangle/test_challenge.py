a4e10866f5dfca1cbb49142442fad59e04abded6
Jim Lawton <jim.lawton@gmail.com>
2018-03-27 21:56:53 +0100

    Add Triangle example tests.

--- lessons/06/triangle/test_challenge.py
+++ lessons/06/triangle/test_challenge.py
@@ -3,5 +3,6 @@ from challenge import solution
 
 
 def test_challenge_array():
-    assert solution([]) == 0
+    assert solution([10, 2, 5, 1, 8, 20]) == 1
+    assert solution([10, 50, 5, 1]) == 0
 
